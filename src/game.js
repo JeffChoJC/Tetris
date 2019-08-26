1d6310489d2023337ea0b3690b0ea41d24794f5a
Jeffrey <chojeffrey@gmail.com>
2019-08-26 10:09:34 -0400

    Changed audio styling

--- src/game.js
+++ src/game.js
@@ -5,9 +5,11 @@ class Game {
         this.board = new Board(ctx);
         this.started = false;
         this.paused = false;
+        this.music = new Audio('./assets/theme_song.mp3');
         
         let modal = document.getElementsByClassName('controls-background');
         document.getElementById('close-controls-button').onclick = () => modal[0].classList.toggle('hidden');
+        document.getElementById('mute-check').onchange = () => this.mute();
         document.getElementById('play-button').addEventListener("click", () => this.start(ctx));
         document.getElementById('restart').addEventListener("click", () => this.restart(ctx));
         document.addEventListener("keydown", e => this.pause(e, ctx));
@@ -18,6 +20,9 @@ class Game {
             this.board.speed = setInterval(() => this.board.drop(ctx), 820);
             this.started = true;
             this.board.controlsEnabled = true;
+
+            this.music.loop = true;
+            this.music.play();
         }
     }
 
@@ -37,6 +42,16 @@ class Game {
         if (confirm("Are you sure you want to restart?")) {
             location.reload();
         }	
+    }
+
+    mute() {
+        let checked = document.getElementById('mute-check').checked
+        
+        if (checked === true) {
+            this.music.muted = true;
+        } else {
+            this.music.muted = false;
+        }
     }
 }
 
